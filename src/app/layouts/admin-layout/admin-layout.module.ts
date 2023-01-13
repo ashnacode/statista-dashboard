f06b4818c4c2110a5f6f10574153c3943ce214d2
ashnacode <ehsan.re786@gmail.com>
2023-01-13 08:18:00 +0100

    removed menu

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -14,11 +14,7 @@ import { AspectLeftComponent } from '../../components/aspect-left/aspect-left.co
 import { AspectRightComponent } from '../../components/aspect-right/aspect-right.component';
 import { BenchmarkLeftComponent } from '../../components/benchmark-left/benchmark-left.component';
 import { BenchmarkRightComponent } from '../../components/benchmark-right/benchmark-right.component';
-import { AspectChartLeftComponent } from '../../components/aspect-chart-left/aspect-chart-left.component';
-import { AspectChartRightComponent } from '../../components/aspect-chart-right/aspect-chart-right.component';
-
 import { BenchmarkingComponent } from '../../benchmarking/benchmarking.component';
-import { ChartRadarComponent } from '../../components/chart-radar/chart-radar.component';
 import { ChartAspectComponent } from '../../widgets/chart-aspect/chart-aspect.component';
 import { ReviewsComponent } from '../../widgets/reviews/reviews.component';
 import { NgChartsModule } from 'ng2-charts';
@@ -41,10 +37,7 @@ import { NgChartsModule } from 'ng2-charts';
     AspectRightComponent,
     BenchmarkLeftComponent,
     BenchmarkRightComponent,
-    AspectChartLeftComponent,
-    AspectChartRightComponent,
     BenchmarkingComponent,
-    ChartRadarComponent,
     ChartAspectComponent,
     ReviewsComponent,
   ]
